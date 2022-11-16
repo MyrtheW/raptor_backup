eca3072b00b5dc4370dced395852d32eaede35f5
MyrtheW <willemsenmyrthe@gmail.com>
2022-11-16 16:12:24 +0100

    15/11 g++-10: internal compiler error.

---
+++ src/update/load_hibf.cpp
@@ -0,0 +1,45 @@
+#include <raptor/search/search_single.hpp>
+#include <raptor/update/load_hibf.hpp>
+#include <raptor/update/insertions.hpp>
+#include "raptor/build/store_index.hpp"
+
+
+namespace raptor
+{
+template <bool compressed>
+void load_hibf(upgrade_arguments const & arguments)
+{
+    using index_structure_t = std::conditional_t<compressed, index_structure::hibf_compressed, index_structure::hibf>;
+    auto index = raptor_index<index_structure_t>{}; // Does not do anything with arguments? Strangely seems only done in store_index.
+    double index_io_time{0.0};
+    load_index(index, arguments, index_io_time); // add: arguments.parts - 1, if HIBF consists of multiple
+    // prepare index to allow for updates
+    index.ibf().user_bins.initialize_filename_position_to_ibf_bin();
+    index.ibf().initialize_previous_ibf_id(); //
+    // create datastructure for perfect ns.
+
+    if constexpr (not compressed){ // should be constexpr, otherwise it will try for all vlaues of compressed
+        update_hibf(arguments, index); // currently requires uncompressed type?  requires (compressed == false)
+    }
+    else{ // todo try uncompressing a IBF
+        auto some_compressed_ibf = index.ibf().ibf_vector[0];
+        seqan3::interleaved_bloom_filter<seqan3::data_layout::compressed> ibf_uncompressed{some_compressed_ibf};
+    }
+
+    // if arguments.compressed, it should be compressed again
+    store_index(arguments.in_file, std::move(index), arguments); // store index
+}
+
+template void load_hibf<false>(upgrade_arguments const & arguments);
+template void load_hibf<true>(upgrade_arguments const & arguments);
+
+}
+
+
+// for each of the bins in bin_paths: (maybe sort? or do this smartly somehow?
+
+
+// request_ibf_idx
+// request_user_bin_idx
+
+// when using such an existing function, make sure the arguments have the right arguments type, by using a template (see store_index.hpp how to) , typename arguments_t
